c7eb2f8b38cf67c6caaecf70b02b8ba7806a41b2
MIMIK <78480793+I-vanik@users.noreply.github.com>
2025-02-05 12:25:50 +0300

    Add adaptive

---
+++ js/burger-menu.js
@@ -0,0 +1,23 @@
+let btn = $('.btn');
+
+btn.on('click', function() {
+  $(this).toggleClass('active not-active');
+});
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Закройте выпадающее меню, если пользователь щелкает за его пределами
+  window.onclick = function(event) {
+    if (!event.target.matches('.btn')) {
+      var dropdowns = document.getElementsByClassName("box-list");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
