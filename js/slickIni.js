20fd0429cbd22cb5f2b4fdbfaa1fe1390837e01e
MIMIK <78480793+I-vanik@users.noreply.github.com>
2025-03-18 20:07:37 +0300

    Add files via upload

--- js/slickIni.js
+++ js/slickIni.js
@@ -1,30 +1,18 @@
 $(document).ready(function () {
   $(".slider").slick({
     arrows: true,
-    slidesToShow: 3,
+    slidesToShow: 2,
     slidesToScroll: 2,
+    // centerMode: true,
     responsive:[
+
         {
-            breakpoint: 1300,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 2,
-            }
-        },
-        {
-            breakpoint: 1040,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2,
-            }
-        },
-        {
-            breakpoint: 735,
+            breakpoint: 1238,
             settings: {
                 slidesToShow: 1,
                 slidesToScroll: 1,
             }
-        }
+        },
     ]
   });
 });
