f1d70cd1c9ae27179f1ee2a3bb3bda63063989a0
MIMIK <78480793+I-vanik@users.noreply.github.com>
2025-04-29 18:54:41 +0300

    Add files via upload

--- js/animation.js
+++ js/animation.js
@@ -1,7 +1,10 @@
+const teamEl1 = document.querySelector('.team-info-img')
+
 function onEntry(entry) {
     entry.forEach(change => {
       if (change.isIntersecting) {
        change.target.classList.add('element-show');
+       teamEl1.classList.add('el1')
       } else {
         // change.target.classList.remove('element-show');
       }
@@ -9,10 +12,14 @@ function onEntry(entry) {
   }
   
   let options = {
-    threshold: [0.5] };
+    threshold: [0.7] };
   let observer = new IntersectionObserver(onEntry, options);
   let elements = document.querySelectorAll('.element-animation');
   
   for (let elm of elements) {
     observer.observe(elm);
   }
+
+  anime({
+    
+  });
---
+++ js/scroll.js
@@ -0,0 +1,10 @@
+// $(document).ready(function(){
+//     $(document).scroll(function(evt){
+//     var v2 = Math.abs($('.team').position().top - $(window).height()/2);
+//     var v1 = $(this).scrollTop();
+//         if( v1 > v2 ){
+//       console.log('in');
+//         $('.team').addClass('animated flip')
+//       }
+//     });
+//     });
